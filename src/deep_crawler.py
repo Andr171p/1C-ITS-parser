df9415e850c7d9414afddd45bad6717ad50f22a1
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-04-21 14:32:53 +0500

    21.04.2025

---
+++ src/deep_crawler.py
@@ -0,0 +1,47 @@
+import json
+import logging
+import asyncio
+
+from crawl4ai import AsyncWebCrawler, CrawlerRunConfig
+from crawl4ai.deep_crawling import BFSDeepCrawlStrategy
+from crawl4ai.content_scraping_strategy import LXMLWebScrapingStrategy
+from crawl4ai.deep_crawling.filters import FilterChain, URLPatternFilter  # , DomainFilter
+from crawl4ai.deep_crawling.filters import ContentRelevanceFilter
+
+from src.settings import BASE_DIR
+
+
+async def main() -> None:
+    # domain_filter = DomainFilter(allowed_domains=["https://v8.1c.ru/erp/"])
+    relevance_filter = ContentRelevanceFilter(
+        query="Какая стоимость внедрения 1с ERP",
+        threshold=0.7  # Minimum similarity score (0.0 to 1.0)
+    )
+    filter_chain = FilterChain([URLPatternFilter(patterns=["*erp*"]), relevance_filter])
+    config = CrawlerRunConfig(
+        deep_crawl_strategy=BFSDeepCrawlStrategy(max_depth=1, filter_chain=filter_chain),
+        scraping_strategy=LXMLWebScrapingStrategy(),
+        verbose=True
+    )
+
+    async with AsyncWebCrawler() as crawler:
+        results = await crawler.arun("https://v8.1c.ru/erp/", config=config)
+
+        print(f"Crawled {len(results)} pages in total")
+
+        file_path = BASE_DIR / "parsed_data" / "answer.json"
+        with open(file_path, "w", encoding="utf-8") as file:
+            pages = []
+            for result in results:
+                page = {
+                    "url": result.url,
+                    "content": result.markdown[3050:]
+                }
+                pages.append(page)
+            data = json.dumps(pages, ensure_ascii=False)
+            file.write(data)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
